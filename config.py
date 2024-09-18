ede64e997efdf30bec6c6084174fe9624ac0252c
angelaCassi <127158569+angelaCassi@users.noreply.github.com>
2024-09-18 16:02:22 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 #Define a configuração do Flask e do SQLAlchemy.
 print("Config.py is being read correctly.")
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///clinica.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///clinica.db'  # Banco de dados SQLite
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Desativa mensagens de modificação
